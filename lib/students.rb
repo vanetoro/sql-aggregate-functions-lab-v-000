1cd0b000d0060961c138e3a6e51620e4c2576799
Vanessa Toro <vanetoro84@gmail.com>
2018-04-21 20:08:30 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,6 +1,8 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
+
+  "SELECT MAX (gps) FROM STUDENTS"
 end
 
 def lowest_student_gpa
